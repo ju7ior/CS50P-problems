6d08ff99a35eae51aa294491631e6ccf6fdd04ca
Elpidio Junior <117687576+ju7ior@users.noreply.github.com>
2023-01-01 23:05:44 -0500

    Create einstein.py

---
+++ problem set 0/einstein.py
@@ -0,0 +1,12 @@
+# Take mass input and use it to calculate E = mc^2
+
+# Take input as int
+mass = int(input("Mass(kg): "))
+# Define formula function
+def formula(mass):
+    E = mass * pow(300000000, 2)
+    return E
+# Call function
+result = formula(mass)
+# Print result with commas
+print(f"{result: ,}")
